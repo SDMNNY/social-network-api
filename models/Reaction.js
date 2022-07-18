47396b1555b6d0dc1acea4bd382bfec54a70fa27
sdmnny <sdmnny@gmail.com>
2022-07-18 18:27:42 -0400

    reaction model completed

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require("mongoose");
+const date = require("date-and-time");
+
+const reactionSchema = new Schema( 
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Data,
+            default: Date.now,
+            get: (time) => date.format(time, "YYYY/MM/DD at HH:mm:ss"),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema; 
